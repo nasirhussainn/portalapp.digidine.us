59fab6f6516a46b7f723cccab3f3439a2cfd1fed
Nasir <nasirhussaintormik@gmail.com>
2025-09-04 08:39:36 +0500

    Update user status route; modify endpoint to use userId parameter and link to the updateUserStatus controller

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -9,5 +9,5 @@ router.get("/get-premium-users", accountController.getPremiumUsers);
 router.delete("/delete/:user_id", accountController.deleteAccount);
 router.put("/update-profile", upload.single("profileImage"), accountController.updateProfile);
 router.patch("/update-premium-status", accountController.updatePremiumStatus);
-router.patch(`/account/update-status/${userId}`, { status });
+router.patch(`/account/update-status/:userId`, accountController.updateUserStatus);
 module.exports = router;
